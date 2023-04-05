12f33085b41d798eb2cd54d2b0e1dcbb68acd60e
Blagoja Hristoski <blagoja.hristoski@contractor.evgo.com>
2023-04-05 16:17:43 +0200

    waitInRul

--- tests/home/home.test.ts
+++ tests/home/home.test.ts
@@ -22,5 +22,5 @@ Scenario('Verify that user is correctly navigated to Style guide page', async ({
   await basePage.signInDashboard()
   await homePage.styleGuideButtonClick()
   await I.switchToNextTab()
-  await I.seeInCurrentUrl('dashboard-staging.plugshare.com/assets/data_entry_style_guide_for_plugshare_dashboard')
+  await I.waitInUrl('dashboard-staging.plugshare.com/assets/data_entry_style_guide_for_plugshare_dashboard', basePage.timeoutSec)
 }).tag('@dashboard').tag('@HomeTests').tag('@C607680')
